e90291c57d483b6463a5d586d129d60a8df919c9
Alberto VG <betovel99@gmail.com>
2021-07-21 15:30:41 -0500

    Reversed_String

---
+++ reversed_string.py
@@ -0,0 +1,13 @@
+def solution(string):
+    return string[::-1]  # Esta es la clave para invertir texto.
+
+
+msg = solution("hola")
+
+print(msg)
+# _________________________________________
+
+txt = "hola mundo"
+print(txt[::-1])
+#__________________________________________
+
